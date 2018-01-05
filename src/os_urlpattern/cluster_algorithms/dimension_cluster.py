332df6a89ca989045c884ccdf2b652b8c028a2b1
root <cfhamlet@gmail.com>
2018-01-05 17:07:58 +0800

    load cluster from config file

---
+++ src/os_urlpattern/cluster_algorithms/dimension_cluster.py
@@ -0,0 +1,30 @@
+class Cluster(object):
+    pass
+
+
+class PieceCluster(Cluster):
+    pass
+
+
+class LengthCluster(Cluster):
+    pass
+
+
+class FuzzyCluster(Cluster):
+    pass
+
+
+class BasePatternCluster(Cluster):
+    pass
+
+
+class MixedPatternCluster(Cluster):
+    pass
+
+
+class LastDotSplitCluster(Cluster):
+    pass
+
+
+def cluster(config, url_meta, piece_pattern_tree):
+    pass
