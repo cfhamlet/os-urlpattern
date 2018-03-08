5fef131fa80ac872a9b6f997ce896daf1173e2f4
root <cfhamlet@gmail.com>
2018-03-08 20:06:14 +0800

    daily update

--- src/os_urlpattern/__init__.py
+++ src/os_urlpattern/__init__.py
@@ -5,5 +5,10 @@ import pkgutil
 __version__ = pkgutil.get_data(__package__, 'VERSION').decode('ascii').strip()
 version_info = tuple(int(v) if v.isdigit() else v
                      for v in __version__.split('.'))
+
+if sys.version_info < (2, 6):
+    print("os-urlpattern %s requires Python 2.6" % __version__)
+    sys.exit(1)
+
 del pkgutil
 del sys
